121f5d32c43a22bceb1a52021232bb4659bf7be4
dual <info@getdual.com>
2012-10-08 05:36:57 -0700

    added gallery-downloader-2.sh

--- gallery-downloader-2.sh
+++ gallery-downloader-2.sh
@@ -5,7 +5,7 @@
 for i in {1..100}; do
 	PIC=1
 	while true; do
-		wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1" http://www.example.com/gallery$i/pic_$PIC.jpg
+		wget http://www.example.com/gallery$i/pic_$PIC.jpg
 		file pic_$PIC.jpg | grep HTML
 		if [ $? -eq 0 ]; then
 			rm pic_$PIC.jpg
@@ -13,6 +13,6 @@ for i in {1..100}; do
 		fi
 		mv pic_${PIC}.jpg ${i}_${PIC}.jpg
 		PIC=$((PIC+1))
-		sleep 2
+		sleep 1
 	done
 done
