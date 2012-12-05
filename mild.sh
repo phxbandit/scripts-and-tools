838bdecb78268cb6fc533d91377b6e51d47c61ac
dual <info@getdual.com>
2012-12-05 11:41:02 -0700

    improved mild.sh, hosts-plus.txt, and iso8601 for packet storm submission

--- mild.sh
+++ mild.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-# mild.sh - Subdomain brute forcer inspired by fierce.pl
-# by dual
+# mild.sh 0.5 - Subdomain brute forcer inspired by fierce.pl
+# by dual (whenry)
 #
 # Usage: ./mild.sh <-s X> DOMAIN NAMESERVER
 # -s = Sleep X number of seconds
 #
-# Uses subdomain list, hosts.txt, from:
+# hosts-plus.txt based on hosts.txt from:
 # http://ha.ckers.org/fierce/hosts.txt
 
-echo "mild.sh - A subdomain brute forcer inspired by fierce.pl"
+# Include time and data functions
+. iso8601
 
-# Check for sleep, domain and nameserver arguments
+# Main query function
+query() {
+	# Log output
+	exec > >(tee $DOM-$SH_TIME.log)
+
+	# Perform dig query
+	for i in $(cat rand-hosts.txt); do
+		dig +noall +answer $i.$DOM @$NAM
+		if [ $CHECK ]; then sleep $SLEEP; fi
+	done
+}
+
+# Check for minimum arguments
 if [ $# -ne 2 ]; then
 	if [ $# -ne 4 ]; then
-		echo "Usage: ./mild.sh <-s X> DOMAIN NAMESERVER"
+		echo "mild.sh - Subdomain brute forcer"
+		echo "Usage: $0 <-s X> DOMAIN NAMESERVER"
 		echo "-s = Sleep X number of seconds"
 		exit;
 	fi
 fi
 
+# Output banner
+echo "Starting mild.sh ( https://github.com/getdual ) at $TIME"
+
+# Check for sleep
 if [ "$1" == "-s" ]; then
 	if [ $2 -eq $2 ]; then
 		echo "  Sleeping $2 seconds between queries."
@@ -45,7 +63,7 @@ type dig || {
 }
 
 # Check for subdomains list
-if [ -f hosts.txt ]; then
+if [ -f hosts-plus.txt ]; then
 	echo "  Good, we have a subdomain list."
 else
 	echo "  No subdomain list. Fetching."
@@ -54,22 +72,19 @@ else
 		echo "  No wget. Forget it."
 		exit;
 	}
-	wget http://ha.ckers.org/fierce/hosts.txt
+	wget https://raw.github.com/getdual/scripts-n-tools/master/hosts-plus.txt
 fi
 
 # Randomize subdomains
 echo "  Randomizing subdomains."
 if [ $(uname) = "Darwin" ]; then
-	for i in $(cat hosts.txt); do echo "$RANDOM $i"; done | sort | sed -E 's/^[0-9]+ //' > rand_hosts.txt
+	for i in $(cat hosts-plus.txt); do echo "$RANDOM $i"; done | sort | sed -E 's/^[0-9]+ //' > rand-hosts.txt
 else
-	sort -R hosts.txt > rand_hosts.txt
+	sort -R hosts-plus.txt > rand-hosts.txt
 fi
 
 echo "  Brute forcing subdomains of $DOM using nameserver, $NAM."
 echo
 
-# Perform queries
-for i in $(cat rand_hosts.txt); do
-	dig +noall +answer $i.$DOM @$NAM
-	if [ $CHECK ]; then sleep $SLEEP; fi
-done
+# Call main function
+query
