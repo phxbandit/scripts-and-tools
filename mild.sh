8cc95649b1842f01e1a11e55c11e85cf353a9667
dual <info@getdual.com>
2012-12-05 13:36:22 -0700

    improved mild.sh, hosts-plus.txt, and iso8601 for packet storm submission

--- mild.sh
+++ mild.sh
@@ -8,6 +8,13 @@
 #
 # hosts-plus.txt based on hosts.txt from
 # http://ha.ckers.org/fierce/hosts.txt
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# dual (@getdual) wrote gallerycgi. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. dual
+# ----------------------------------------------------------------------------
 
 # Include time and date functions
 . iso8601
