1631746a218c3d14b426b33598aad6f519a54058
dual <info@getdual.com>
2012-12-05 13:40:02 -0700

    fixed beer-ware license in spiga.py and sshscan.py

--- spiga.py
+++ spiga.py
@@ -7,7 +7,7 @@
 #
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
-# dual (@getdual) wrote gallerycgi. As long as you retain this notice you
+# dual (@getdual) wrote spiga.py. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return. dual
 # ----------------------------------------------------------------------------
--- sshscan.py
+++ sshscan.py
@@ -25,7 +25,7 @@
 #
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
-# dual (@getdual) wrote gallerycgi. As long as you retain this notice you
+# dual (@getdual) wrote sshscan.py. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return. dual
 # ----------------------------------------------------------------------------
