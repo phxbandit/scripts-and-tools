d1061c2b61c79873b95ba009c7987c7b85816510
dual <info@getdual.com>
2013-03-01 14:43:03 -0700

    added logging to bangrab.sh

--- bangrab.sh
+++ bangrab.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# bangrab.sh - Simple banner grabber
+# bangrab.sh - Simple HTTP banner grabber
 # by dual
+
+. iso8601
+
+LOG=bangrab-$SH_TIME.log
 
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 <nmap-compatible IP range>"
@@ -13,9 +17,9 @@ nmap -n -p80 -oG .bangrabtemp1 $1
 grep open .bangrabtemp1 | awk '{print $2}' > .bangrabtemp2
 
 for i in $(cat .bangrabtemp2); do
-	echo $i
-	echo "HEAD / HTTP/1.1" | nc -w 5 $i 80
-	echo
+	echo $i >> $LOG
+	echo "HEAD / HTTP/1.1" | nc -w 5 $i 80 2>&1 >> $LOG
+	echo >> $LOG
 done
 
 rm -f .bangrabtemp*
