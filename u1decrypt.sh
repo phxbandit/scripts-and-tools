0b615c2730014905645251837d6efb8b8b66aa17
dual <github@getdual.neomailbox.net>
2013-08-21 04:33:00 -0700

    fixed tab size

--- u1decrypt.sh
+++ u1decrypt.sh
@@ -14,63 +14,63 @@ echo
 
 # Check for argument
 if [ $# -ne 1 ]; then
-	echo "u1decrypt.sh needs an encrypted file as an argument... exiting."
-	exit 1
+    echo "u1decrypt.sh needs an encrypted file as an argument... exiting."
+    exit 1
 else
-	target="$1"
+    target="$1"
 fi
 
 # Verify sync directory
 [ -d "$ubuntu1" ] || {
-	echo "$ubuntu1 directory not found... exiting."
-	exit 1
+    echo "$ubuntu1 directory not found... exiting."
+    exit 1
 }
 
 # Verify openssl is installed
 if [ "$(which openssl)" = "" ]; then
-	echo "openssl not found... exiting."
-	exit 1
+    echo "openssl not found... exiting."
+    exit 1
 fi
 
 # Get decryption passphrase
 read -p "Enter the decryption passphrase: " pass
 if [[ "$pass" = "" ]]; then
-	echo "A passphrase is required to decrypt data... exiting."
-	exit 1
+    echo "A passphrase is required to decrypt data... exiting."
+    exit 1
 fi
 
 # Copy and decrypt
 if [[ "$target" =~ \.tar\.gz\.des3 ]]; then
-	echo "Decrypting $target..."
-	filename=$(basename "$target" .des3)
-	dirname=$(basename "$filename" .tar.gz)
-	openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename"
-	if [ -e "$dirname" ]; then
-		echo "Directory $dirname exists."
-		read -p "Do you want to overwrite $dirname [y/n]? " yorn
-		if [[ "$yorn" = 'y' || "$yorn" = 'Y' ]]; then
-			tar xzf "$HOME/$filename"
-			rm "$HOME/$filename"
-		else
-			echo "Exiting so as to not overwrite $dirname."
-			rm "$HOME/$filename"
-			exit 1
-		fi
-	else
-		tar xzf "$HOME/$filename"
-		rm "$HOME/$filename"
-	fi
+    echo "Decrypting $target..."
+    filename=$(basename "$target" .des3)
+    dirname=$(basename "$filename" .tar.gz)
+    openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename"
+    if [ -e "$dirname" ]; then
+        echo "Directory $dirname exists."
+        read -p "Do you want to overwrite $dirname [y/n]? " yorn
+        if [[ "$yorn" = 'y' || "$yorn" = 'Y' ]]; then
+            tar xzf "$HOME/$filename"
+            rm "$HOME/$filename"
+        else
+            echo "Exiting so as to not overwrite $dirname."
+            rm "$HOME/$filename"
+            exit 1
+        fi
+    else
+        tar xzf "$HOME/$filename"
+        rm "$HOME/$filename"
+    fi
 elif [[ "$target" =~ \.des3 ]]; then
-	echo "Decrypting $target..."
-	filename=$(basename "$target" .des3)
-	if [ -e "$HOME/$filename" ]; then
-		openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename-1"
-	else
-		openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename"
-	fi
+    echo "Decrypting $target..."
+    filename=$(basename "$target" .des3)
+    if [ -e "$HOME/$filename" ]; then
+        openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename-1"
+    else
+        openssl des3 -d -salt -pass pass:"$pass" -in "$target" -out "$HOME/$filename"
+    fi
 else
-	echo "$target is not encrypted file... exiting."
-	exit 1
+    echo "$target is not encrypted file... exiting."
+    exit 1
 fi
 
 echo
--- u1encrypt.sh
+++ u1encrypt.sh
@@ -14,50 +14,50 @@ echo
 
 # Check for argument
 if [ $# -ne 1 ]; then
-	echo "u1encrypt.sh needs a file or directory as an argument... exiting."
-	exit 1
+    echo "u1encrypt.sh needs a file or directory as an argument... exiting."
+    exit 1
 else
-	target="$1"
+    target="$1"
 fi
 
 # Verify sync dir
 [ -d "$ubuntu1" ] || {
-	echo "$ubuntu1 directory not found... exiting."
-	exit 1
+    echo "$ubuntu1 directory not found... exiting."
+    exit 1
 }
 
 # Verify openssl is installed
 if [ "$(which openssl)" = "" ]; then
-	echo "openssl not found... exiting."
-	exit 1
+    echo "openssl not found... exiting."
+    exit 1
 fi
 
 # Get and confirm encryption passphrase
 while [[ "$match" = "" ]]; do
-	read -p "Please enter the encryption passphrase:   " pass
-	read -p "Please confirm the encryption passphrase: " pass_conf
-	if [[ "$pass" = "" ]]; then
-		echo "Passphrase is empty... exiting."
-		exit 1
-	elif [[ "$pass" = "$pass_conf" ]]; then
-		match='1'
-		continue
-	else
-		echo "Passphrases did not match."
-	fi
+    read -p "Please enter the encryption passphrase:   " pass
+    read -p "Please confirm the encryption passphrase: " pass_conf
+    if [[ "$pass" = "" ]]; then
+        echo "Passphrase is empty... exiting."
+        exit 1
+    elif [[ "$pass" = "$pass_conf" ]]; then
+        match='1'
+        continue
+    else
+        echo "Passphrases did not match."
+    fi
 done
 
 # Encrypt file or dir
 if [ -d "$target" ]; then
-	dirname=$(basename "$target")
-	echo "Encrypting $dirname..."
-	tar czf "$dirname.tar.gz" "$dirname"
-	openssl des3 -salt -pass pass:"$pass" -in "$dirname.tar.gz" -out "$ubuntu1/$dirname.tar.gz.des3"
-	rm "$dirname.tar.gz"
+    dirname=$(basename "$target")
+    echo "Encrypting $dirname..."
+    tar czf "$dirname.tar.gz" "$dirname"
+    openssl des3 -salt -pass pass:"$pass" -in "$dirname.tar.gz" -out "$ubuntu1/$dirname.tar.gz.des3"
+    rm "$dirname.tar.gz"
 else
-	filename=$(echo "$target" | sed 's/.*\///g')
-	echo "Encrypting $filename..."
-	openssl des3 -salt -pass pass:"$pass" -in "$target" -out "$ubuntu1/$target.des3"
+    filename=$(echo "$target" | sed 's/.*\///g')
+    echo "Encrypting $filename..."
+    openssl des3 -salt -pass pass:"$pass" -in "$target" -out "$ubuntu1/$target.des3"
 fi
 
 echo
