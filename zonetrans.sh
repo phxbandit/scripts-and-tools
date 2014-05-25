0aa18a0fd540506f84f9e514694b0c66aff6b82b
dual <github@getdual.neomailbox.net>
2014-05-25 12:21:55 -0700

    updated zonetrans

--- zonetrans.sh
+++ zonetrans.sh
@@ -2,10 +2,10 @@
 # zonetrans.sh - Attempts full zone transfer from
 #                each authoritative name server
 
-if [ $# -ne 1 ]; then
+[ $# -eq 1 ] || {
     echo "usage: zonetrans example.com"
     exit 1
-fi
+}
 
 echo
 for i in $(dig ns $1 +short | sed -e 's/\.$//'); do
