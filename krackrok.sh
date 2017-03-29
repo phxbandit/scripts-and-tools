ff80c33aad9652a5098a37e32d414b7fa8e070df
WSTNPHX <info@sctn9.com>
2017-03-29 13:19:34 -0700

    added krackrok.sh for removing old kernels on ubuntu

---
+++ krackrok.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kernpack=()
+curkern=$(uname -r | awk -F'-generic' '{print $1}')
+for i in $(dpkg -l | grep 'linux-' | awk '{print $2}' | egrep '[[:digit:]]\.[[:digit:]]\.[[:digit:]]' | grep -v "$curkern"); do
+    kernpack+=($i)
+done
+printf "\nCurrent kernel: $(uname -r)\n\n"
+apt purge "${kernpack[@]}"
