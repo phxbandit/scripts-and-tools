a87f256a6294f8caf2ac1206bb561e3a01b178bb
WSTNPHX <info@sctn9.com>
2017-05-16 09:41:30 -0700

    fixed grep bug

--- krackrok.sh
+++ krackrok.sh
@@ -2,7 +2,7 @@
 # VVestron Phoronix
 kernpack=()
 curkern=$(uname -r | awk -F'-generic' '{print $1}')
-for i in $(dpkg -l | grep 'linux-' | awk '{print $2}' | egrep '[[:digit:]]\.[[:digit:]]\.[[:digit:]]' | grep -v "$curkern"); do
+for i in $(dpkg -l | grep 'linux-' | awk '{print $2}' | grep -P '\d\.\d{1,2}\.\d' | grep -v "$curkern"); do
     kernpack+=($i)
 done
 printf "\nCurrent kernel: $(uname -r)\n\n"
