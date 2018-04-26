ec51109e297980925f515da93ff28e40aad71057
WSTNPHX <info@sctn9.com>
2018-04-26 08:56:50 -0700

    added selfdestruct.pw data scripts

---
+++ dataget.php
@@ -0,0 +1,47 @@
+<?php
+
+// dataget.php - Retrieves file from selfdestruct.pw hashes
+
+// Check the environment
+if ( ! function_exists('curl_init') ) {
+    die("ERROR: cURL not available\n");
+}
+
+if ( ! isset($argv[1]) ) {
+    die("ERROR: Provide hash file as argument\n");
+} else {
+    $file = $argv[1];
+}
+
+// Function to retrieve data for each hash
+function getData($hash) {
+    $query = '?' . $hash;
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://sctn9.com/selfdestruct/' . trim($query));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    $regex = '/PASSWORD\:<\/b>(.*?)<br \/>/';
+    preg_match($regex, $response, $match);
+    print_r( html_entity_decode($match[1]) );
+}
+
+// Read hash file and call getData for each hash
+$fh = fopen($file, "r") or die("ERROR: Unable to open hash file\n");
+
+$sha256regex = '/^[0-9a-f]{64}$/';
+
+while ( !feof($fh) ) {
+    $line = fgets($fh);
+    if ( preg_match($sha256regex, $line) ) {
+        getData($line);
+    }
+}
+
+fclose($fh);
---
+++ dataput.php
@@ -0,0 +1,53 @@
+<?php
+
+// dataput.php - Stores arbitrary files in selfdestruct.pw
+
+// Check the environment
+if ( ! function_exists('curl_init') ) {
+    die("ERROR: cURL not available\n");
+}
+
+if ( ! isset($argv[1]) ) {
+    die("ERROR: Provide file as argument\n");
+} else {
+    $file = $argv[1];
+}
+
+// Function to get hash for each 256 byte chunk
+function getHash($str) {
+    $passData = $str;
+    $data = array('password' => $passData, 'submit' => 'Submit');
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, "https://sctn9.com/selfdestruct/");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    $regex = '/[0-9a-f]{64}/';
+    preg_match($regex, $response, $match);
+    print_r($match[0]);
+    echo "\n";
+}
+
+// Read, normalize, and get hash for file 256 bytes at a time
+$fh = fopen($file, "r") or die("ERROR: Unable to open file\n");
+
+$newlines = array("\r", "\n", "\r\n");
+
+while ( !feof($fh) ) {
+    $strTemp1 = fread($fh, 256);
+    $strTemp2 = str_replace($newlines, " ", $strTemp1);
+    $strTemp3 = str_replace("\t", " ", $strTemp2);
+    $str = preg_replace("/\s+/", " ", $strTemp3);
+    getHash($str);
+}
+
+fclose($fh);
+
