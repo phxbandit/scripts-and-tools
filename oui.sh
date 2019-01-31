5ca3a81fc6fa79bd6270cbcec84aba6f45695a40
WSTNPHX <WSTNPHX@users.noreply.github.com>
2019-01-30 17:07:19 -0700

    verifies MAC format

--- oui.sh
+++ oui.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # oui.sh - IEEE OUI lookup
 
-if [ $# -ne 1 ]; then
+help() {
     echo "Usage: oui.sh <MAC address>"
     exit 1
+}
+
+if [ $# -ne 1 ]; then
+    help
 fi
 
 tmp_mac="$1"
 ieee='http://standards-oui.ieee.org/oui.txt'
 txt="$HOME/bin/oui.txt"
 
-mac=$(echo "$tmp_mac" | cut -b 1-8 | sed -e 's/\:/-/g' | tr [:lower:] [:upper:])
+if [[ "$tmp_mac" =~ ^([0-9A-Fa-f]{2}(:|-)){5}[0-9A-Fa-f]{2}$ ]]; then
+    mac=$(echo "$tmp_mac" | cut -b 1-8 | sed -e 's/\:/-/g' | tr [:lower:] [:upper:])
+elif [[ "$tmp_mac" =~ ^[0-9A-Fa-f]{12}$ ]]; then
+    mac=$(echo "$tmp_mac" | cut -b 1-6 | tr [:lower:] [:upper:])
+else
+    help
+fi
 
 if [ ! -e "$txt" ]; then
     echo "Retreiving oui.txt from ieee.org..."
@@ -24,6 +34,4 @@ else
     fi
 fi
 
-printf "Searching for $mac...\n\n"
 grep "$mac" "$txt"
-echo
