c0cdfd5442e0f0967f8d36f76ef2b6093c2d2b32
WSTNPHX <WSTNPHX@users.noreply.github.com>
2019-02-22 06:22:46 -0700

    added ftps configs for wp

--- wp-config-ftps.php
+++ wp-config-ftps.php
@@ -1,6 +1,6 @@
 define('FS_METHOD', 'direct');
 define('FTP_SSL', true);
 define('FTP_HOST', 'host.or.ip.address');
-define('FTP_USER', 'ftps');
+define('FTP_USER', 'user');
 define('FTP_PASS', 'password');
 define('FTP_BASE', '/path/to/wordpress/');
