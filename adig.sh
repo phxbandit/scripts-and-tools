0220e6b8717019537936083d141f495d7da9d5d6
phxbandit <875444+phxbandit@users.noreply.github.com>
2024-12-04 09:28:04 -0700

    Update adig.sh

--- adig.sh
+++ adig.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-# adig.sh - Queries authoritative name servers
-#           for A, www, and TXT records
 
-IFS=$'\n'
-
-[ $# -eq 1 ] || {
+if [ $# -ne 1 ]; then
     echo "usage: adig example.com"
     exit 1
-}
+fi
 
-cname="www.$1"
+echo -e "\ndig +short $1 @8.8.8.8"
+ipaddr=$(dig +short "$1" @8.8.8.8)
+echo "$ipaddr"
 
+echo -e "\ndig +short -x $ipaddr"
+dig +short -x "$ipaddr" | head -1
+
+echo -e "\ndig +short -t ns $1"
+dig -t ns "$1" +short
 echo
-for i in $(dig ns $1 +short | sed -e 's/\.$//'); do
-    echo "$i"
-    for j in $(dig "$1" @"$i" +short); do
-        echo "@ A $j"
-    done
-    www=$(dig "$cname" @"$i" +short | head -1)
-    echo "www $www"
-    for k in $(dig "$1" @"$i" txt +short); do
-        echo "@ TXT $k"
-    done
-    echo
-done
